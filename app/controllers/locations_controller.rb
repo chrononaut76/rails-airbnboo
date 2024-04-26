aa989d8a98ec0f5af760fbed0acfcf0017fc52c6
andrew godeke <legodeke@hotmail.com>
2024-04-25 21:18:58 -0400

    Add LocationsController with basic CRUD actions

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,57 @@
+class LocationsController < ApplicationController
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
+
+  # GET /locations
+  def index
+    @locations = Location.all
+  end
+
+  # GET /locations/:id
+  def show
+  end
+
+  # GET /locations/new
+  def new
+    @location = Location.new
+  end
+
+  # POST /locations
+  def create
+    @location = Location.new(location_params)
+
+    if @location.save
+      redirect_to @location, notice: 'Location was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # GET /locations/:id/edit
+  def edit
+  end
+
+  # PATCH/PUT /locations/:id
+  def update
+    if @location.update(location_params)
+      redirect_to @location, notice: 'Location was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /locations/:id
+  def destroy
+    @location.destroy
+    redirect_to locations_url, notice: 'Location was successfully destroyed.'
+  end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :address, :description, :price, :image_url)
+  end
+end
