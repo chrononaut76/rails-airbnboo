55006f66df301791eebbf6ce55e6197eea010338
andrew godeke <legodeke@hotmail.com>
2024-04-25 20:17:25 -0400

    Add Location model with validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,10 @@
 class Location < ApplicationRecord
   belongs_to :user
+
+  # Validations
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :image_url, presence: true
 end
