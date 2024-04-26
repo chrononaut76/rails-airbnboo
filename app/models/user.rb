46c5a283a6fa44aa14454d7ca336da48d98a3772
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-25 21:48:42 -0500

    Created User model and views with Devise, added columns to db, added validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :locations
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :is_host, presence: true
 end
