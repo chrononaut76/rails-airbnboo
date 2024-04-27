0b9404523a9deb18722ef51312edb78f498ae322
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-27 11:20:54 -0500

    restoring location model and adding db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Clearing db'
+rails db:reset
+
+8.times do
+  new_location = Location.new(
+    name: 'Gingerbread House',
+    address: 'Deep in the woods',
+    description: 'An edible lodging made of your favorite sweets from childhood!',
+    price: 100.0,
+    image_url: 'gingerbread_house.jpg',
+    user_id: 1
+    )
+    puts "Seeding new location: #{new_location.name}"
+end
