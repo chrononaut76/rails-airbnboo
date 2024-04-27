a86f637d31a9e3bf1f81f05f5799d1615dcacd54
andrew godeke <legodeke@hotmail.com>
2024-04-27 12:35:35 -0400

    removed validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,2 @@
 class Location < ApplicationRecord
-  # Associations
-  belongs_to :user
-
-  # Validations
-  validates :name, presence: true
-  validates :price, presence: true
-  validates :image_url, presence: true
 end
