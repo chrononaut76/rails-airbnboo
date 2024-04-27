c4a08ab439ad0a47380344bbb297d2d7e6851512
karim issa <issa61139@gmail.com>
2024-04-27 17:35:40 -0400

    implemented the grid css to show the location,title,description, and price also gerardo and i fixed something in seeds file to save the locations into the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
+user = User.create!(
+  email:'issa@gmail.com',
+  password:'123456',
+  first_name:'Issa',
+  last_name:'Diop')
 8.times do
-  new_location = Location.new(
+  new_location = Location.create!(
     name: 'Gingerbread House',
     address: 'Deep in the woods',
     description: 'An edible lodging made of your favorite sweets from childhod!',
     price: 100.0,
-    image_url: 'images/gingerbread_house.jpg',
-    user_id: 1
+    image_url: 'gingerbread_house.jpg',
+    user_id: user.id
   )
   puts "Created new location #{new_location.name}"
 end
