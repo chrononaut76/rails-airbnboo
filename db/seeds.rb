c74c17dc848ae9cfa8f8bbeb48d113c4cebdae61
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-27 12:40:28 -0500

    Created seed.db with 8 locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+8.times do
+  new_location = Location.new(
+    name: 'Gingerbread House',
+    address: 'Deep in the woods',
+    description: 'An edible lodging made of your favorite sweets from childhod!',
+    price: 100.0,
+    image_url: 'images/gingerbread_house.jpg',
+    user_id: 1
+  )
+  puts "Created new location #{new_location.name}"
+end
