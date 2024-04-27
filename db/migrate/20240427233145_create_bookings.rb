7b4634de43095dfc9d8988b9ea39ebd569efb973
gabriel alvarez <gabrieleli099@gmail.com>
2024-04-27 12:22:04 -0400

    added create locations and booking, change time stamps

--- db/migrate/20240427233145_create_bookings.rb
+++ db/migrate/20240427233145_create_bookings.rb
@@ -2,6 +2,8 @@ class CreateBookings < ActiveRecord::Migration[7.1]
   def change
     create_table :bookings do |t|
       t.boolean :status
+      t.references :user, null: false, foreign_key: true
+      t.references :location, null: false, foreign_key: true
 
       t.timestamps
     end
