7250084d5650aa1fb4efecf2bc7bf290fa4e2352
karim issa <issa61139@gmail.com>
2024-04-29 12:26:04 -0400

    implemented soem changes to the location pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_27_233145) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_27_233146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.boolean "status"
     t.bigint "user_id", null: false
     t.bigint "location_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
     t.index ["location_id"], name: "index_bookings_on_location_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
