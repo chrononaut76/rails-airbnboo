cab428aafabe8140bea514824aabc09c6592f47a
karim issa <issa61139@gmail.com>
2024-05-02 00:40:50 -0400

    fixed the km issue in config geocoder file

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -16,7 +16,7 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-  units: :ki,                 # :km for kilometers or :mi for miles
+  units: :km,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 
   # Cache configuration
