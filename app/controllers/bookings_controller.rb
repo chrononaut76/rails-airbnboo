987dc916f48b4d606e39f0a089f88a882a8b4007
gabriel alvarez <gabrieleli099@gmail.com>
2024-05-02 18:53:30 -0400

    added create 'new' and 'create' bookings and reseeded

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,17 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @location = Location.find(params[:location_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @location = Location.find(params[:location_id])
+    @booking.location = @location
+    @booking.save
+    redirect_to location_path(@location)
+  end
+
   def edit
     @booking = Booking.find(params[:id])
   end
@@ -9,9 +22,9 @@ class BookingsController < ApplicationController
   #   redirect_to booking_path(@booking)
   # end
 
-  # private
+  private
 
-  # def booking_params
-  #   params.require(:booking).permit(:status)
-  # end
+  def booking_params
+    params.require(:booking).permit(:status)
+  end
 end
