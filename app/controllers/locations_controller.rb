2b5ed4a0ed8c00074d4170bc1eeb578a66973a84
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-05-04 15:44:37 -0500

    Adding logic to permit actions if is_host = true

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,11 @@ class LocationsController < ApplicationController
 
   # GET /locations
   def index
-    @locations = Location.all
+    if current_user.is_host
+      @locations = Location.where(user: current_user)
+    else
+      @locations = Location.all
+    end
     @markers = @locations.geocoded.map do |location|
       {
         lat: location.latitude,
@@ -15,19 +19,26 @@ class LocationsController < ApplicationController
 
   # GET /locations/:id
   def show
-    @booking = Booking.new
-    @pending_booking = Booking.find_by(user: current_user, location: params[:id])
+    if current_user.is_host
+      @booking = Booking.find_by(location: params[:id])
+    else
+      @booking = Booking.new
+      @pending_booking = Booking.find_by(user: current_user, location: params[:id])
+    end
   end
 
   # GET /locations/new
   def new
-    @location = Location.new
+    if current_user.is_host
+      @location = Location.new
+    else
+      redirect_to root, status: :unauthorized
+    end
   end
 
   # POST /locations
   def create
     @location = Location.new(location_params)
-
     if @location.save
       redirect_to @location, notice: 'Location was successfully created.', status: :created
     else
@@ -37,6 +48,9 @@ class LocationsController < ApplicationController
 
   # GET /locations/:id/edit
   def edit
+    unless current_user.is_host
+      redirect_to root, status: :unauthorized
+    end
   end
 
   # PATCH/PUT /locations/:id
@@ -50,8 +64,12 @@ class LocationsController < ApplicationController
 
   # DELETE /locations/:id
   def destroy
-    @location.destroy
-    redirect_to locations_url, notice: 'Location was successfully destroyed.', status: :see_other
+    if current_user.is_host
+      @location.destroy
+      redirect_to locations_url, notice: 'Location was successfully destroyed.', status: :see_other
+    else
+      redirect_to root, status: :unauthorized
+    end
   end
 
   private
